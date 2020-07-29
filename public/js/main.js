55e34023fe1a72fe8dcc8d064dcf88f3a8c18603
Pavan Kumar v <pavankumar.vip888@gmail.com>
2020-07-29 02:47:29 -0700

    initial commit

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+document
+.querySelector(".menu-btn")
+.addEventListener("click", () =>
+    document.querySelector(".hamburger ").classList.toggle("is-active")
+);
+document
+.querySelector(".menu-btn")
+.addEventListener("click", () =>
+    document.querySelector(".main-menu").classList.toggle("is-active")
+);
+
