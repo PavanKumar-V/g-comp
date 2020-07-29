78fb8a7de375ce513abfe5adefb1c1eabe55cecb
Pavan Kumar v <pavankumar.vip888@gmail.com>
2020-07-29 05:25:56 -0700

    initial commit

--- public/js/main.js
+++ public/js/main.js
@@ -8,4 +8,14 @@ document
 .addEventListener("click", () =>
     document.querySelector(".main-menu").classList.toggle("is-active")
 );
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollpos = window.pageYOffset;
+    if(prevScrollpos > currentScrollpos) {
+        document.getElementById("navbar").style.top = "0";
 
+    } else {
+        document.getElementById("navbar").style.top = "-100px";
+    }
+    prevScrollpos = currentScrollpos
+}
